5082ccc860ce85f9cd5ac53063159e56d6c8a8b9
Yunus96 <mohammadyunus102@gmail.com>
2024-10-17 15:28:11 +0530

    adding more logic

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./Header/Header.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx"
+import Container from "./container/Container.jsx"
+
+export {
+    Header,
+    LogoutBtn,
+    Container
+}
